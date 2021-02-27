d0f6f89d9e95cc0623884452bc5f1408a1b8a60d
charromax <manuelrg88@gmail.com>
2021-02-27 11:45:07 -0300

    activate device onboarding
    Signed-off-by: charromax <manuelrg88@gmail.com>

---
+++ app/src/main/java/com/example/gardenbotapp/ui/onboarding/activatepages/PageTwoFragment.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2021. Created by charr0max  -> manuelrg88@gmail.com
+ */
+
+package com.example.gardenbotapp.ui.onboarding.activatepages
+
+import androidx.fragment.app.Fragment
+
+class PageTwoFragment : Fragment()
